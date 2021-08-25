6495ae0953682fe6c6dd1ea0c545110d32eca303
anastaga07 <anastaga07@gmail.com>
2021-08-25 16:53:46 +0300

    Add negative checks

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,10 @@
 from pages.main_page import MainPage
 from pages.product_page import ProductPage
+import time
 
 import pytest
-
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+#link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+"""@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
@@ -13,17 +14,41 @@ import pytest
                                   pytest.param ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",marks=pytest.mark.xfail(reason="some bug")),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser,link):
+def test_guest_can_add_product_to_basket(browser):
 #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-#link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019?promo=offer0"
+    link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019?promo=offer0"
     page =ProductPage(browser, link)
     page.open()
     page = ProductPage(browser, browser.current_url)
     page.add_poduct_to_basket()
     page.solve_quiz_and_get_code()   
     page.message_items_should_be_add_to_basket()
-    page.cost_should_be_eql_price()
-    
+    page.cost_should_be_eql_price()"""
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page =ProductPage(browser,link)
+    page.open()
+    page.add_poduct_to_basket()
+    page. should_not_be_success_message_is_not_element_present()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page =ProductPage(browser,link)
+    page.open()
+    page.should_not_be_success_message_is_not_element_present()
+
+@pytest.mark.skip
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page =ProductPage(browser,link)
+    page.open()
+    page.add_poduct_to_basket()
+    time.sleep(1)
+    page.should_not_be_success_message()
+
+
 
 
 
