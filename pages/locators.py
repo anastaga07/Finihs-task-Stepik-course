ba86de0c9dcd7804e3b401dd99fe00992659f172
anastaga07 <anastaga07@gmail.com>
2021-08-25 16:54:38 +0300

    Add basket page

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,22 @@ class MainPageLocators():
 class LoginPageLocators ():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    PRODUCT_ADD_BUTTON= (By.CSS_SELECTOR, "button.btn-add-to-basket")   
+    MESSAGE_AFTER_ADD_ITEM = (By.CSS_SELECTOR, '.alert-success:first-child .alertinner strong')
+    TITLE_OF_THE_ITEM = (By.CSS_SELECTOR, 'h1')
+    PRICE_ITEM = (By.CSS_SELECTOR, '.product_main .price_color')
+    BASKET_TOTAL = (By.CSS_SELECTOR, '.alert-info .alertinner strong')
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, 'div.alertinner')
+
+class BasePageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".basket-mini a")
+
+
+
+class BasketPageLocators():
+    PRODUCT_IN_BASKET = (By.CSS_SELECTOR, "#content_inner h2")
+   # EMPTY_BASKET = (By.CSS_SELECTOR, "#content_inner p")
+    MESSAGE_IN_BASKET = (By.CSS_SELECTOR, '#content_inner')
+
