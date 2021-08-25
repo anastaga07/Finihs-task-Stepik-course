13ff6a7b92357fa0bd8a5a58c42b8a3bdc618f93
anastaga07 <anastaga07@gmail.com>
2021-08-25 16:52:57 +0300

    Add negative checks

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,11 +1,15 @@
 from .base_page import BasePage
-from .locators import ProductPageLocator
+from .locators import ProductPageLocators
 from selenium.common.exceptions import NoAlertPresentException 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
 import math
 
 class ProductPage(BasePage): 
   def add_poduct_to_basket(self):
-        add_to_cart = self.browser.find_element(*ProductPageLocator.PRODUCT_ADD_BUTTON)
+        add_to_cart = self.browser.find_element(*ProductPageLocators.PRODUCT_ADD_BUTTON)
         add_to_cart.click()
 
   def solve_quiz_and_get_code(self):
@@ -24,19 +28,22 @@ class ProductPage(BasePage):
 
   def message_items_should_be_add_to_basket(self):
     """Проверка: название товара в сообщении совпадает с добавленным товаром"""
-    title_of_item = self.browser.find_element(*ProductPageLocator.TITLE_OF_THE_ITEM).text
-    message_after_add = self.browser.find_element(*ProductPageLocator.MESSAGE_AFTER_ADD_ITEM).text
+    title_of_item = self.browser.find_element(*ProductPageLocators.TITLE_OF_THE_ITEM).text
+    message_after_add = self.browser.find_element(*ProductPageLocators.MESSAGE_AFTER_ADD_ITEM).text
     assert title_of_item == message_after_add, 'book title does not match message'
 
   def cost_should_be_eql_price(self):
     """Проверка: стоимость корзины равна цене товара"""
-    price_item = self.browser.find_element(*ProductPageLocator.PRICE_ITEM).text
-    basket_total = self.browser.find_element(*ProductPageLocator.BASKET_TOTAL).text
+    price_item = self.browser.find_element(*ProductPageLocators.PRICE_ITEM).text
+    basket_total = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL).text
     assert price_item == basket_total, 'price of items does not eql basket total'
  
-  def should_not_be_success_message(self):
-        """Проверка, что элемент не появился на странице"""
-        assert self.is_not_element_present(*ProductPageLocator.SUCCESS_MESSAGE), \
+  def should_not_be_success_message_is_not_element_present(self):
+    """Проверка, что элемент не появился на странице"""
+    assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
             'Success message is presented, but should not be'
 
-  
+  def should_not_be_success_message(self):
+    """Проверка, что элемент исчезает на странице"""
+    assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+         "Success message is presented, but should not be"
