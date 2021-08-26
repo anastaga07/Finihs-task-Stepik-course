3d3a8c157b8980354fde7fcff732757327e48404
anastaga07 <anastaga07@gmail.com>
2021-08-26 11:05:47 +0300

    final edits

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
+import time
 
 
 class LoginPage(BasePage):
@@ -9,13 +10,22 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        """Проверка: current_url содержит слово login"""
-        assert 'login' in self.browser.current_url, 'login not in current url'
+        assert 'login' in self.browser.current_url, 'url is invalid, does not contain "login"'
 
     def should_be_login_form(self):
-        """Проверка: форма логина представлена на странице"""
-        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'Login form is not presented'
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'no login form on the page'
 
     def should_be_register_form(self):
-        """Проверка: форма регистрации представлена на странице"""
-        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), 'Register form is not presented'
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), 'no registration form on the page'
+
+    def register_new_user(self):
+        email = str(time.time()) + "@mail.com"
+        password = '123@_abc'
+        email_form = self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL)
+        password_form_1 = self.browser.find_element(*LoginPageLocators.PASSWORD_FORM_1)
+        password_form_2 = self.browser.find_element(*LoginPageLocators.PASSWORD_FORM_2)
+        email_form.send_keys(email)
+        password_form_1.send_keys(password)
+        password_form_2.send_keys(password)
+        button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        button.click()
