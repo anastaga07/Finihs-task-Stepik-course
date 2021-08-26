e6ed795dfee643734414067e5cc024ba4df36068
anastaga07 <anastaga07@gmail.com>
2021-08-26 11:05:07 +0300

    final edits

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -3,19 +3,10 @@ from .locators import BasketPageLocators
 
 
 class BasketPage(BasePage):
-    def should_be_empty_basket(self):
-        """Проверка на пустую корзину"""
-        self.guest_cant_see_product_in_basket()
-        self.guest_can_see_empty_basket()
-    
 
-    def guest_cant_see_product_in_basket(self):
-        """Проверка: не должно быть товаров в корзине"""
-        assert self.is_not_element_present(*BasketPageLocators.PRODUCT_IN_BASKET), \
-            "Product in basket is presented, but should not be"
+    def basket_has_no_goods(self):
+        assert self.element_is_not_present(*BasketPageLocators.ITEMS_TO_BUY_NOW), 'Basket not empty'
 
-    def guest_can_see_empty_basket(self):
-        """Проверка: есть сообщение корзина пуста"""
-        text_message_in_basket = self.browser.find_element(*BasketPageLocators.MESSAGE_IN_BASKET).text
-        assert 'Your basket is empty' in text_message_in_basket, \
-            'The basket not exist the message that basket is empty'
+    def has_empty_basket_text(self):
+        text = self.browser.find_element(*BasketPageLocators.EMPTY_BASKET_TEXT).text
+        assert text == 'Your basket is empty. Continue shopping', 'Page does not have text about empty basket'
