5d053021c4acf504867c8dc1d83f1cc24517f370
anastaga07 <anastaga07@gmail.com>
2021-08-26 10:33:53 +0300

    final edits

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,8 @@
-﻿from .pages.main_page import MainPage
-from .pages.basket_page import BasketPage
-from .pages.login_page import LoginPage
-import pytest
+﻿import pytest
+from pages.main_page import MainPage
+from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
+
 
 link = "http://selenium1py.pythonanywhere.com/"
 def test_guest_can_go_to_login_page1(browser):
@@ -9,21 +10,28 @@ def test_guest_can_go_to_login_page1(browser):
     page.open()                      # открываем страницу
     page.go_to_login_page()         # выполняем метод страницы — переходим на страницу логина
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
 
 @pytest.mark.login_guest
-def test_guest_can_go_to_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+class TestLoginFromMainPage():
+                        
+    def test_guest_can_go_to_login_page(self, browser):     
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
+
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+
+       
 def test_guest_can_see_login_form(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
     page = LoginPage(browser, link)
